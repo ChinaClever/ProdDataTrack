b845e4ce3bed7022599337684c07af0f5d1da95b
chenwanyue <luozhiyong131@qq.com>
2024-09-24 14:36:08 +0800

    模块质检网页

---
+++ ui/yudao-ui-admin-vue3-master/src/api/bus/moduleTest/index.ts
@@ -0,0 +1,9 @@
+import request from '@/config/axios'
+
+export const ModuleTestApi = {
+
+  getModuleTestPage: async (params: any) => {
+    return await request.get({ url: `/bus/moduleTest`, params })
+  },
+
+}
