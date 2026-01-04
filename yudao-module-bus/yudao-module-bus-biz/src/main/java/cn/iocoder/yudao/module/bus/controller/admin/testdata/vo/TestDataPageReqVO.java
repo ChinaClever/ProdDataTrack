f9743743600b34c8a1a5d9873ace94b3f94c8941
chenwanyue <luozhiyong131@qq.com>
2026-01-04 15:36:10 +0800

    添加字段注释
    添加字段注释

--- yudao-module-bus/yudao-module-bus-biz/src/main/java/cn/iocoder/yudao/module/bus/controller/admin/testdata/vo/TestDataPageReqVO.java
+++ yudao-module-bus/yudao-module-bus-biz/src/main/java/cn/iocoder/yudao/module/bus/controller/admin/testdata/vo/TestDataPageReqVO.java
@@ -1,6 +1,7 @@
 package cn.iocoder.yudao.module.bus.controller.admin.testdata.vo;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
@@ -8,19 +9,19 @@ public class TestDataPageReqVO{
     private Integer pageNo;
 
     private Integer pageSize;
-
+    @Schema(description = "订单号", requiredMode = Schema.RequiredMode.REQUIRED, example = "1")
     private String orderId;
-
+    @Schema(description = "成品代码", requiredMode = Schema.RequiredMode.REQUIRED, example = "1")
     private String productSN;
-
+    @Schema(description = "模块序列号", requiredMode = Schema.RequiredMode.REQUIRED, example = "1")
     private String moduleSn;
-
+    @Schema(description = "测试项目", requiredMode = Schema.RequiredMode.REQUIRED, example = "1")
     private String testItem;
-
+    @Schema(description = "测试要求", requiredMode = Schema.RequiredMode.REQUIRED, example = "1")
     private String testRequest;
-
+    @Schema(description = "测试结果", requiredMode = Schema.RequiredMode.REQUIRED, example = "1")
     private String testResult;
-
+    @Schema(description = "语言", requiredMode = Schema.RequiredMode.REQUIRED, example = "1")
     private String language;
 
     private String[] timeRange;
--- yudao-module-bus/yudao-module-bus-biz/src/main/java/cn/iocoder/yudao/module/bus/entity/TestData.java
+++ yudao-module-bus/yudao-module-bus-biz/src/main/java/cn/iocoder/yudao/module/bus/entity/TestData.java
@@ -3,6 +3,7 @@ package cn.iocoder.yudao.module.bus.entity;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.ToString;
 
@@ -15,33 +16,33 @@ public class TestData {
      */
     @TableId(value = "id", type = IdType.AUTO)
     private Long id;
-
+    @Schema(description = "成品代码", requiredMode = Schema.RequiredMode.REQUIRED, example = "1")
     private String productSn;
-
+    @Schema(description = "模块序列号", requiredMode = Schema.RequiredMode.REQUIRED, example = "1")
     private String moduleSn;
 
     private String softVersion;
 
     private String languageSelect;
-
+    @Schema(description = "订单号", requiredMode = Schema.RequiredMode.REQUIRED, example = "1")
     private String orderId;
 
     private String orderNum;
-
+    @Schema(description = "设备名称", requiredMode = Schema.RequiredMode.REQUIRED, example = "1")
     private String devName;
 
     private String toolName;
-
+    @Schema(description = "测试结果", requiredMode = Schema.RequiredMode.REQUIRED, example = "1")
     private String testResult;
 
     private String startTime;
 
     private String testStep;
-
+    @Schema(description = "测试项目", requiredMode = Schema.RequiredMode.REQUIRED, example = "1")
     private String testItem;
 
     private String testCfg;
-
+    @Schema(description = "测试要求", requiredMode = Schema.RequiredMode.REQUIRED, example = "1")
     private String testRequest;
 
     private String testProcess;
